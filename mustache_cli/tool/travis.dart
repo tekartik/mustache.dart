d12f81ee8cb7c2b693b84cb4ebb25df998d2c87b
alex <alex@tekartik.com>
2020-05-01 00:14:44 +0200

    test: dart 2.9 formatting

--- mustache_cli/tool/travis.dart
+++ mustache_cli/tool/travis.dart
@@ -1,4 +1,5 @@
 import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
   var shell = Shell();
@@ -6,8 +7,11 @@ Future main() async {
   await shell.run('''
 # Analyze code
 dartanalyzer --fatal-warnings --fatal-infos .
-dartfmt -n --set-exit-if-changed .
-
 pub run test -p vm
 ''');
+
+  // Formatting change int 2.9 with hashbang first line
+  if (dartVersion >= Version(2, 9, 0, pre: '0')) {
+    await shell.run('dartfmt -n --set-exit-if-changed .');
+  }
 }
