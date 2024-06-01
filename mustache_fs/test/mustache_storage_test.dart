741f2347f040b2498d1fb37e052206029c63e510
Alexandre Roux <alex@tekartik.com>
2024-06-01 12:26:28 +0200

    fix ci

--- mustache_fs/test/mustache_storage_test.dart
+++ mustache_fs/test/mustache_storage_test.dart
@@ -1,4 +1,6 @@
 @TestOn('vm')
+library;
+
 import 'package:tekartik_firebase_local/firebase_local.dart';
 import 'package:tekartik_firebase_storage_fs/storage_fs_io.dart';
 import 'package:tekartik_mustache_fs/mustache_storage.dart';
